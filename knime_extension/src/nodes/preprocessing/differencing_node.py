eabe4d523a346a2856d1a2e81f5666d99197f0c6
Ali Marvi <Ali.Marvi@knime163.kn.knime.com>
2023-10-19 15:35:12 +0200

    Black formatting

--- knime_extension/src/nodes/preprocessing/differencing_node.py
+++ knime_extension/src/nodes/preprocessing/differencing_node.py
@@ -13,12 +13,21 @@ __category = knext.category(
     name="Preprocessing",
     description="Nodes for pre-processing date&time.",
     # starting at the root folder of the extension_module parameter in the knime.yml file
-    icon="icons/icon.png"
+    icon="icons/icon.png",
 )
 
 
-@knext.node(name="Differencing", node_type=knext.NodeType.MANIPULATOR, icon_path="icons/icon.png", category=__category, id="differencing")
-@knext.input_table(name="Input Data", description="Table contains the regression column to be diffeenced")
+@knext.node(
+    name="Differencing",
+    node_type=knext.NodeType.MANIPULATOR,
+    icon_path="icons/icon.png",
+    category=__category,
+    id="differencing",
+)
+@knext.input_table(
+    name="Input Data",
+    description="Table contains the regression column to be diffeenced",
+)
 @knext.output_table(name="Aggregations", description="Output the column having ")
 class SeasonalDifferencingNode:
     """
@@ -26,25 +35,26 @@ class SeasonalDifferencingNode:
     """
 
     diff_params = SeasonalDifferencingParams()
-        
-    def configure(self, configure_context:knext.ConfigurationContext, input_schema):
-       
-       self.diff_params.target_column = kutil.column_exists_or_preset(
-            configure_context, self.diff_params.target_column, input_schema, kutil.is_numeric
+
+    def configure(self, configure_context: knext.ConfigurationContext, input_schema):
+        self.diff_params.target_column = kutil.column_exists_or_preset(
+            configure_context,
+            self.diff_params.target_column,
+            input_schema,
+            kutil.is_numeric,
         )
-       
-       return None
-        
-    
+
+        return None
 
     def execute(self, exec_context: knext.ExecutionContext, input_table):
         df = input_table.to_pandas()
 
         target_col = df[self.diff_params.target_column]
 
-        target_col_new = target_col.diff(periods = self.diff_params.lags)
+        target_col_new = target_col.diff(periods=self.diff_params.lags)
 
-        df[self.diff_params.target_column + "(" + (str(-self.diff_params.lags)) + ")"] = target_col_new
+        df[
+            self.diff_params.target_column + "(" + (str(-self.diff_params.lags)) + ")"
+        ] = target_col_new
 
-        return knext.Table.from_pandas(df)        
-
+        return knext.Table.from_pandas(df)
