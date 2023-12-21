7eebc3bdddd0b535118ce0393326a105041c6474
Ali Marvi <35396741+dsaam94@users.noreply.github.com>
2023-12-21 12:43:30 +0100

    Updated typo in Timestamp Aligner doc string.

--- knime_extension/src/nodes/preprocessing/timestamp_alignment_node.py
+++ knime_extension/src/nodes/preprocessing/timestamp_alignment_node.py
@@ -32,7 +32,7 @@ class TimestampAlignmentNode:
     Select a timestamp column and a time granularity. The node will verify that a record exists in your table for each value at that granularity, for example if you select hours it will check for 01:00, 02:00, 03:00… if a timestamp is not found it will be inserted and missing values generated for the remaining columns.
     The final output is sorted in ascending order of newly generated timestsamp.
     Use this in combination with the missing value node to correct missing time series data.
-    This node preserves duplicated values and possibly lead to cluttered output. Therefore, in case of duplicated timestamps, we encourage using *Date&Time Granularity* node before using this node.
+    This node preserves duplicated values and possibly lead to cluttered output. Therefore, in case of duplicated timestamps, we encourage using *Date&Time Aggregator* node before using this node.
     """
 
     ts_align_params = TimeStampAlignmentParams()
